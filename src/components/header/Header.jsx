7a5fc806ea0eaa9f17aa0a13cf7bbd492e1734bf
Osakpamukhoko William Ofiare <96081824+kokainaW@users.noreply.github.com>
2023-06-18 17:47:25 +0100

    Update Header.jsx
    Software

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Ofiare Khoko</h1>
-        <h5 className='text-light'>FrontEnd Developer</h5>
+        <h5 className='text-light'>Software Developer</h5>
         <CTA />
         <HeaderSocials />
         <div className="me">
